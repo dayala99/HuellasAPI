888f24762f9947d312bb7314b7c90d56142f1a63
dayala99 <117493354+dayala99@users.noreply.github.com>
2022-11-18 22:10:19 -0500

    Add files via upload

--- src/main/resources/templates/index.js
+++ src/main/resources/templates/index.js
@@ -1,18 +1,10 @@
-const express = require("express");
-const billsRoute = require("./routes/bill");
+import app from './app.js'
+import {PORT} from './config.js'
 
-// settings
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.set("token", process.env.TOKEN || "recipeT0k3n");
-app.listen(PORT, () => console.log(`Escuchando en puerto", ${PORT}`));
 
-// middlewares
-app.use(express.json());
-app.use("/api", billsRoute);
 
-// routes
-app.get("/", (req, res) => {
-  res.send("InfoBase API");
-});
 
+app.listen(PORT)
+console.log('Server running on port', PORT)
+
+ 
